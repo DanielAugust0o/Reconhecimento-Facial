958dde9490d890f8663696e967a66787bb79292d
Daniel Augusto <77458377+UnderClany@users.noreply.github.com>
2023-12-18 20:18:11 -0300

    Update treinamento.py

--- Reconhecimento-Facial-EigenFace/treinamento.py
+++ Reconhecimento-Facial-EigenFace/treinamento.py
@@ -3,8 +3,6 @@ import os
 import numpy as np
 
 eigenface = cv2.face.EigenFaceRecognizer(40, 800)
-fisherface = cv2.face.FisherFaceRecognizer()
-lbph = cv2.face.LBPHFaceRecognizer()
 
 def getImagemComId() :
     caminhos = [os.path.join('fotos', f) for f in os.listdir('fotos')]
